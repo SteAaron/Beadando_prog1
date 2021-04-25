4d8e24e2b075070400eaa10e7da4d1db22ad1faf
SteAaron <78878970+SteAaron@users.noreply.github.com>
2021-04-25 19:13:20 +0200

    Add files via upload

--- A06XT6_8_feladat.py
+++ A06XT6_8_feladat.py
@@ -6,6 +6,39 @@ s = input("Adja meg a fájl nevét: ")
 while True:
     try:
         be = open(s,'r')
+        kimely = open('mely.txt','w')
+        kimagas = open('magas.txt','w')
+        kivegyes = open('vegyes.txt','w')
+
+        for sor in be:
+            # l=sor.rstrip('\n')
+            for szo in sor:
+                if melymgh in szo and magasmgh not in szo:
+                    print(sor, file=kimely)
+                elif melymgh not in szo and magasmgh in szo:
+                    print(sor, file=kimagas)
+                elif melymgh in szo and magasmgh in szo:
+                    print(sor, file=kivegyes)
+
+        be.close()
+        kimely.close()
+        kimagas.close()
+        kivegyes.close()
+
     except FileNotFoundError:
         print('A megadott fájl nem található!')
         s = input("Adjon meg egy új fájl nevet: ")
+
+# for sor in be:
+#     #l=sor.rstrip('\n')
+#     if mely in sor and magas not in sor:
+#         print(sor,file=kimely)
+#     elif mely not in sor and magas in sor:
+#         print(sor,file=kimagas)
+#     elif mely in sor and magas in sor:
+#         print(sor,file=kivegyes)
+#
+# be.close()
+# kimely.close()
+# kimagas.close()
+# kivegyes.close()
