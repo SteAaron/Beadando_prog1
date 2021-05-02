10dbb489b349dcb8eb90ee31b0d85b0a9f7080d5
SteAaron <78878970+SteAaron@users.noreply.github.com>
2021-05-02 21:25:29 +0200

    Add files via upload

--- A06XT6_8_feladat.py
+++ A06XT6_8_feladat.py
@@ -1,44 +1,34 @@
+import sys
+
 melymgh = ['a','á','o','ó','u','ú']
 magasmgh = ['e','é','i','í','ö','ő','ü','ű']
 
 
 s = input("Adja meg a fájl nevét: ")
-while True:
-    try:
-        be = open(s,'r')
-        kimely = open('mely.txt','w')
-        kimagas = open('magas.txt','w')
-        kivegyes = open('vegyes.txt','w')
 
-        for sor in be:
-            # l=sor.rstrip('\n')
-            for szo in sor:
-                if melymgh in szo and magasmgh not in szo:
-                    print(sor, file=kimely)
-                elif melymgh not in szo and magasmgh in szo:
-                    print(sor, file=kimagas)
-                elif melymgh in szo and magasmgh in szo:
-                    print(sor, file=kivegyes)
+try:
+    file = open(s,'r',encoding='utf-8')
+    kimely = open('mely.txt','w')
+    kimagas = open('magas.txt','w')
+    kivegyes = open('vegyes.txt','w')
 
-        be.close()
-        kimely.close()
-        kimagas.close()
-        kivegyes.close()
+    for sor in file:
+        line=sor.strip()
+        for szo in line:
+            if melymgh in szo and not magasmgh in szo:
+                print(sor, file=kimely)
+            elif not melymgh in szo and magasmgh in szo:
+                print(sor, file=kimagas)
+            elif melymgh in szo and magasmgh in szo:
+                print(sor, file=kivegyes)
 
-    except FileNotFoundError:
-        print('A megadott fájl nem található!')
-        s = input("Adjon meg egy új fájl nevet: ")
 
-# for sor in be:
-#     #l=sor.rstrip('\n')
-#     if mely in sor and magas not in sor:
-#         print(sor,file=kimely)
-#     elif mely not in sor and magas in sor:
-#         print(sor,file=kimagas)
-#     elif mely in sor and magas in sor:
-#         print(sor,file=kivegyes)
-#
-# be.close()
-# kimely.close()
-# kimagas.close()
-# kivegyes.close()
+    be.close()
+    kimely.close()
+    kimagas.close()
+    kivegyes.close()
+
+except FileNotFoundError:
+    print('A megadott fájl nem található!')
+    s = input("Adjon meg egy új fájl nevet: ")
+
